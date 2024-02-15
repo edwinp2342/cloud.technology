b3a4583b14010196e8090106f5fb06dbc818663c
edwinp2342 <143545185+edwinp2342@users.noreply.github.com>
2024-02-15 14:43:22 -0500

    Add files via upload

---
+++ Basic input output/excercises/PE3_1.py
@@ -0,0 +1,12 @@
+#Given Code 
+print("012345678901234567890")
+print('A'.rjust(5), 'B'.center(5), 'C'.ljust(5), sep = "")
+#output of 1
+print("""012345678901234567890
+    A  B  C     """)
+#Given Code 
+print(print("01234567890123456"))
+print("{0:^7}{1:4}{2:>6s}".format("one", "two", "three"))
+print(f"{'one':^7}{'two':4}{'three':>6s}")
+
+#Output of 2
